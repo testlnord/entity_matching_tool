513813def1d1c18d95b7b7a082ef0b3db7e0a9fd
Pochatkin <m.a.pochatkin@gmail.com>
2017-04-06 14:50:42 +0300

    added loading of csv files

--- src/js/components/App/App.jsx
+++ src/js/components/App/App.jsx
@@ -3,6 +3,9 @@ import Grid  from 'react-bootstrap/lib/Grid';
 import Nav from 'react-bootstrap/lib/Nav';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import NavItem  from 'react-bootstrap/lib/NavItem';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import Button from 'react-bootstrap/lib/Button';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
 import AddJob from './AddJob';
 import ListJobs from './ListJobs';
 import ServerAPI from '../ServerAPI';
@@ -10,43 +13,51 @@ import './bootstrap.css';
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      jobs: ServerAPI.getListJobs(),
+    constructor() {
+        super();
+        this.state = {
+            jobs: ServerAPI.getListJobs(),
+        };
     };
-  };
   
-  render() {
-    return (
-      <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <span>Entity Matching Tool</span>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-           </Navbar.Collapse>
-        </Navbar>
-        <Grid>
-          <div className="view-jobs">
-            <div className="add-job">
-              <h1>Jobs list</h1>
-              <AddJob/>
+    render() {
+        return (
+            <div>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                          <span>Entity Matching Tool</span>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        {/*<Navbar.Form pullRight>
+                            <FormGroup>
+                                <FormControl type="text" placeholder="Username" />
+                                {' '}
+                                <FormControl type="password" placeholder="Password" />
+                            </FormGroup>
+                            {' '}
+                            <Button bbStyle='succes'>Sign in</Button>
+                        </Navbar.Form>*/}
+                    </Navbar.Collapse>
+                </Navbar>
+                <Grid>
+                    <div className="view-jobs">
+                        <div className="add-job">
+                            <h1>Jobs list</h1>
+                            <AddJob/>
+                        </div>
+                        <div className="list-jobs">
+                            <ListJobs
+                              length={this.state.jobs.length}
+                            />
+                        </div>
+                    </div>
+                </Grid>
             </div>
-            <div className="list-jobs">
-                <ListJobs
-                  length={this.state.jobs.length}
-                />
-            </div>
-          </div>
-        </Grid>
-      </div>
-      
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/js/components/ServerAPI/ServerAPI.jsx
+++ src/js/components/ServerAPI/ServerAPI.jsx
@@ -1,11 +1,15 @@
+import axios from 'axios';
+
 //Templates jobs
 //---------------------------------------------------
 const job1 = {
+	name: "Job 1",
 	request: "Tesla",
 	response: "Tesla tech."
 }
 
 const job2 = {
+	name: "Job 2",
 	request: "Lala",
 	response: "Lalala"
 }
@@ -14,16 +18,31 @@ let list = {
 	jobs: [job1, job2],
 	length: 2
 }
+
+let metrics = ["metric 1", "metric 2"];
 //-----------------------------------------------------
 
+
+let getNameJobs = function() {
+	return list.jobs.map((job) => job.name);
+}
 
 let	getListJobs = function() {
 	//TODO: AJAX post to server
 	return list;
 };
 
-let sendJob = function(self, json) {
-	//TODO: AJAX post
+let getMetrics = function() {
+	//TODO: AJAX post to server
+	return metrics;
+};
+
+let sendJob = function(json, self) {
+	// AJAX post
+	/*axios.post(url, json)
+		.then(function(response) {
+			self.setState({isLoading: false})
+		})*/
 	setTimeout(() => {
     	self.setState({isLoading: false});
 	}, 2000);
@@ -31,5 +50,5 @@ let sendJob = function(self, json) {
 };
 
 
-export default {getListJobs, sendJob};
+export default {getListJobs, getMetrics, getNameJobs, sendJob};
 
