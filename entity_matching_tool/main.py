56b042fe27ee09adf6bce9478fda90289e8593a2
helen sunny <elena.sannikova59@gmail.com>
2017-04-14 21:47:18 +0300

    added script for running server

---
+++ entity_matching_tool/main.py
@@ -0,0 +1,18 @@
+from entity_matching_tool import app, api
+from views import JobList, Jobs, CsvFiles, FieldNames, Entities, Matching
+from flask import render_template
+
+
+api.add_resource(JobList, '/joblist/')
+api.add_resource(Jobs, '/jobs/')
+api.add_resource(CsvFiles, '/csvfiles/')
+api.add_resource(FieldNames, '/fieldnames/')
+api.add_resource(Entities, '/entities/')
+api.add_resource(Matching, '/matching/')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run()
