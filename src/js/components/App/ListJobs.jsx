2a634f6176e1a61c4e931f1d9e929ea82f6dacb0
Pochatkin <m.a.pochatkin@gmail.com>
2017-05-21 21:23:52 +0300

    fix some typos

--- src/js/components/App/ListJobs.jsx
+++ src/js/components/App/ListJobs.jsx
@@ -3,9 +3,6 @@ import Panel from 'react-bootstrap/lib/Panel';
 import PanelGroup  from 'react-bootstrap/lib/PanelGroup';
 import Well from 'react-bootstrap/lib/Well';
 import Button from 'react-bootstrap/lib/Button';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel';
-import FormControl from 'react-bootstrap/lib/FormControl';
 import { browserHistory } from 'react-router';
 import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
 import HelpBlock from 'react-bootstrap/lib/HelpBlock';
@@ -17,26 +14,16 @@ class ListJobs extends Component {
     constructor(props) {
         super();
         this.state = {
-            isChanged: false,
-            metrics: null,
             activeKey: '1',
             listJobs: null,
             url: localStorage.getItem('loginToken') ? 'http://' + localStorage.getItem('loginToken') + ':@localhost:5000' : null
         };
         this.handleSelect = this.handleSelect.bind(this);
         this.deleteJob = this.deleteJob.bind(this);
-        this.changeMetric = this.changeMetric.bind(this);
     };
 
     componentWillMount() {
         let self = this;
-        axios.get('/metrics/')
-            .then(function(response) {
-                self.setState({
-                    metrics: response.data
-                                .map((metric) => <option key={metric} value={metric}>{metric}</option>)
-                });
-            });
         if(localStorage.getItem('loginToken')){
             axios.get(this.state.url + '/joblist/')
                 .then(function(response) {
@@ -71,22 +58,9 @@ class ListJobs extends Component {
                                                 <Well bsSize='sm'>
                                                     {job.metric}
                                                 </Well>
-                                                <FormGroup controlId="metric">
-                                                    <FormControl  componentClass="select" onChange={() => self.setState({ isChanged: true })}>
-                                                        <option value={"..."}> {"..."} </option>
-                                                        {self.state.metrics}
-                                                    </FormControl>
-                                                    <HelpBlock> Change the metric if necessary </HelpBlock>
-                                                </FormGroup>
                                                 <ButtonToolbar>
                                                     <Button onClick={() => browserHistory.push('/options/' + job.id)}> Matching options </Button>
                                                     <Button bsStyle="danger" onClick={() => self.deleteJob(job.id)}> Delete job </Button>
-                                                    <Button 
-                                                        bsStyle='success' 
-                                                        onClick={() => self.changeMetric(job.id)}
-                                                        >  
-                                                        Change metric
-                                                    </Button>
                                                 </ButtonToolbar>
                                             </Panel>
                                         ) 
@@ -103,34 +77,14 @@ class ListJobs extends Component {
             })
     }
 
-    changeMetric(id) {
-        let self = this;
-        const metric = document.getElementById('metric').value
-        if (metric != "...") {
-            axios.post(this.state.url + '/changemetric/', {
-                jobId: id,
-                metric: metric
-            }).then(function(response) {
-                self.setState({
-                    isChanged: false  
-                });
-                console.log(response);
-                self.forceUpdate();
-            });
-        }   
-    }
+    
 
     handleSelect(activeKey) {
-        console.log('Kek');
-        this.setState({
-            isChanged: false 
-        });
         this.state.activeKey === activeKey ? 
             this.setState({ activeKey: null}) : this.setState({ activeKey: activeKey });
     };
 
     render() {
-        console.log(this.state.isChanged);
         return (
             <PanelGroup activeKey={this.state.activeKey} 
                 onSelect={this.handleSelect} accordion>
--- src/js/components/App/Mathcing.jsx
+++ src/js/components/App/Mathcing.jsx
@@ -12,28 +12,38 @@ class Mathcing extends Component {
 	constructor() {
 		super();
 		this.state = {
-			mathcingEntity: {
+			matchingEntity: {
 				name: null,
 				id: null
 			},
+			metrics: null,
 			mathcingEntityEntities: null,
 			url: localStorage.getItem('loginToken') ? 'http://' + localStorage.getItem('loginToken') + ':@localhost:5000' : null
 		};
 		this.match = this.match.bind(this);
+		this.refreshEntities = this.refreshEntities.bind(this);
 
 	}
 
 	componentWillMount() {
 		let self = this;
-		axios.get(this.state.url + "/entities/?jobId=" + this.props.params.id + "&lastEntityId=0")
-            .then(function(response){
-            	console.log(response);
+		axios.get('/metrics/')
+        	.then(function(response) {
+        		console.log(response);
                 self.setState({
-                	mathcingEntity: response.data[0],
-                	listEntities: response.data.slice(1, response.data.length - 1)
-                		.map((entity) => <option key={entity.id} value={entity.id}>{entity.name}</option>)
+                    metrics: response.data
+                                .map((metric) => <option key={metric} value={metric}>{metric}</option>)
                 });
-            })
+            });
+        axios.get(this.state.url + "/entities/?jobId=" + this.props.params.id + "&lastEntityId=0")
+        	.then(function(response){
+	        	/*console.log(response)*/;
+	            self.setState({
+	            	matchingEntity: response.data[0],
+	            	listEntities: response.data.slice(1, response.data.length - 1)
+	            		.map((entity) => <option key={entity.id} value={entity.id}>{entity.name}</option>)
+	            });
+        	});
 	}
 
 	match() {
@@ -45,15 +55,37 @@ class Mathcing extends Component {
 		})
 	}
 
+	refreshEntities() {
+		let self = this;
+		axios.get(this.state.url + "/entities/?jobId=" + this.props.params.id + "&lastEntityId=0")
+            .then(function(response){
+            	/*console.log(response);*/
+                self.setState({
+                	matchingEntity: response.data[0],
+                	listEntities: response.data.slice(1, response.data.length - 1)
+                		.map((entity) => <option key={entity.id} value={entity.id}>{entity.name}</option>)
+                });
+            });
+	}
+
+	changeMetric() {
+        let self = this;
+        axios.post(this.state.url + '/changemetric/', {
+            jobId: this.props.params.id,
+            metric: document.getElementById("metrics").value
+        }).then(function(response) {
+        	self.refreshEntities();
+        });  
+    }
+
 
 	render() {
 		return (
 			<Well> 
-
 				<FormGroup>
 			    	<ControlLabel>Entity from first file</ControlLabel>
 			    	<FormControl.Static>
-			    		{this.state.mathcingEntity.name}
+			    		{this.state.matchingEntity.name}
 			    	</FormControl.Static>
 			    </FormGroup>
 
@@ -63,6 +95,12 @@ class Mathcing extends Component {
                         {this.state.listEntities}
                     </FormControl>
                 </FormGroup>
+
+               	<FormGroup controlId="metrics">
+               		<FormControl componentClass="select" onChange={() => this.changeMetric()}>
+               			{this.state.metrics} 
+               		</FormControl>
+               	</FormGroup>
 
                 <Button bsStyle='success' onClick={() => this.match()}> Matching </Button>
 			</Well>
