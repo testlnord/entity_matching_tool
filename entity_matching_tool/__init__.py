93091d9de7f20bd6d1964b3cb587121f57927b03
Aleksey Pauls <aleksey.pauls@mail.ru>
2017-11-22 16:15:25 +0700

    Branch with mongodb to operate with entities and matched entities

--- entity_matching_tool/__init__.py
+++ entity_matching_tool/__init__.py
@@ -3,16 +3,19 @@ from logging import FileHandler
 import psycopg2
 import sqlalchemy
 from sqlalchemy_utils import database_exists, create_database
+from mongoengine import *
 
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
+from .config import test_config
 from .config import app_config
 
 app = Flask("Entity matching tool")
-app.config.from_object(app_config)
-db = SQLAlchemy(app)
+app.config.from_object(test_config)
+# app.config.from_object(app_config)
+db = SQLAlchemy(app)  # object for the database management
 api = Api(app)
 
 handler = FileHandler('log_file')
@@ -22,16 +25,33 @@ handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 
 try:
+    mongo = connect(test_config.MONGO['db'])
     conn = psycopg2.connect("dbname='{}' user='{}' "
-                            "host='{}' password='{}'".format(app_config.POSTGRES['db'],
-                                                             app_config.POSTGRES['user'],
-                                                             app_config.POSTGRES['host'],
-                                                             app_config.POSTGRES['pw']))
+                            "host='{}' password='{}'".format(test_config.POSTGRES['db'],
+                                                             test_config.POSTGRES['user'],
+                                                             test_config.POSTGRES['host'],
+                                                             test_config.POSTGRES['pw']))
 except psycopg2.OperationalError as e:
-    engine = sqlalchemy.create_engine("postgres://{}:{}@{}/{}".format(app_config.POSTGRES['user'],
-                                                                      app_config.POSTGRES['pw'],
-                                                                      app_config.POSTGRES['host'],
-                                                                      app_config.POSTGRES['db']))
+    engine = sqlalchemy.create_engine("postgres://{}:{}@{}/{}".format(test_config.POSTGRES['user'],
+                                                                      test_config.POSTGRES['pw'],
+                                                                      test_config.POSTGRES['host'],
+                                                                      test_config.POSTGRES['db']))
     if not database_exists(engine.url):
         create_database(engine.url)
+
+
+# try:
+#     mongo = connect(app_config.MONGO['db'], host=app_config.MONGO['host'])
+#     conn = psycopg2.connect("dbname='{}' user='{}' "
+#                             "host='{}' password='{}'".format(app_config.POSTGRES['db'],
+#                                                              app_config.POSTGRES['user'],
+#                                                              app_config.POSTGRES['host'],
+#                                                              app_config.POSTGRES['pw']))
+# except psycopg2.OperationalError as e:
+#     engine = sqlalchemy.create_engine("postgres://{}:{}@{}/{}".format(app_config.POSTGRES['user'],
+#                                                                       app_config.POSTGRES['pw'],
+#                                                                       app_config.POSTGRES['host'],
+#                                                                       app_config.POSTGRES['db']))
+#     if not database_exists(engine.url):
+#         create_database(engine.url)
 
